017482405c663a107fa0934a6af381c4235859bc
Persinus <nguyenmanh2004devgame@gmail.com>
2025-08-09 20:02:20 +0700

    Cập nhật TouchToStartUIManager.cs

--- Assets/Script/TouchToStartUIManager.cs
+++ Assets/Script/TouchToStartUIManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class TouchToStartUIManager : MonoBehaviour
 {
@@ -9,14 +11,41 @@ public class TouchToStartUIManager : MonoBehaviour
     {
         if (hasStarted) return;
 
-        if (Input.GetMouseButtonDown(0)) // chạm hoặc click
+#if UNITY_EDITOR || UNITY_STANDALONE
+        if (Input.GetMouseButtonDown(0))
         {
-            startUI.SetActive(false); // ẩn UI
-            hasStarted = true;
-
-           
+            HandleClick(Input.mousePosition);
         }
-       
+#else
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            HandleClick(Input.GetTouch(0).position);
+        }
+#endif
     }
 
+    void HandleClick(Vector2 screenPos)
+    {
+        // Nếu chạm vào UI thì thoát luôn, không làm gì
+        if (IsPointerOverUI(screenPos))
+            return;
+
+        // Nếu chạm vùng xanh -> tắt UI và bắt đầu game
+        startUI.SetActive(false);
+        hasStarted = true;
+        LevelManager.Instance.StartGame();
+    }
+
+    bool IsPointerOverUI(Vector2 pos)
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current)
+        {
+            position = pos
+        };
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+
+        return results.Count > 0;
+    }
 }
